e80d355fe7fcd183dddd7ecfa5010ada88a0f192
Samany Cutrim <saah.cutrim@gmail.com>
2026-03-23 10:55:28 -0300

    Admin: update team members

--- html/Falaw/falaw.com.br/team-data.js
+++ html/Falaw/falaw.com.br/team-data.js
@@ -13,8 +13,8 @@ var FALAW_TEAM = [
     "name": "Camila Vasconcelos",
     "email": "ferrazandrade@falaw.com.br",
     "role": "Assistente Administrativa",
-    "bio": "Profissional da área administrativa, com formação técnica em Secretariado pelo Centro Paula Souza e atualmente em processo de graduação em Administração pela Universidade Anhembi Morumbi. Possui sólida base em organização, gestão de processos e suporte executivo, aliando conhecimento técnico à formação acadêmica em desenvolvimento..",
-    "status": "active"
+    "bio": "Profissional da área administrativa, com formação técnica em Secretariado pelo Centro Paula Souza e atualmente em processo de graduação em Administração pela Universidade Anhembi Morumbi. Possui sólida base em organização, gestão de processos e suporte executivo, aliando conhecimento técnico à formação acadêmica em desenvolvimento.",
+    "status": "memoriam"
   },
   {
     "id": "mn1yd8t2ymn4",
