20e33269c16dc3c41e696ed6166b83a8a51f6826
Samany Cutrim <saah.cutrim@gmail.com>
2026-04-29 19:16:28 -0300

    Admin: update Supabase config

--- html/Falaw/falaw.com.br/sb-config.js
+++ html/Falaw/falaw.com.br/sb-config.js
@@ -1,4 +1,3 @@
 /* sb-config.js — gerado pelo admin Falaw. */
 window.SB_URL='https://yleofidqkimeanpuothv.supabase.co';
 window.SB_KEY='sb_publishable_Lz028EaqbPI-84U8wRLJhQ_zKHb9tKD';
-window.BACKEND_API_URL=window.BACKEND_API_URL||'';
