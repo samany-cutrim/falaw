167ae5b1f66d57df3127cb947f4f985387167ab9
nevada-mirror <nevada@mirror.local>
2026-05-08 16:24:26 -0300

    feat: exportar/restaurar configuracoes via bookmarklet; salvar n8n e admin email no sb-config.js

--- html/Falaw/falaw.com.br/sb-config.js
+++ html/Falaw/falaw.com.br/sb-config.js
@@ -2,3 +2,5 @@
 window.SB_URL='https://yleofidqkimeanpuothv.supabase.co';
 window.SB_KEY='sb_publishable_Lz028EaqbPI-84U8wRLJhQ_zKHb9tKD';
 window.BACKEND_API_URL='https://falaw.onrender.com';
+window.SB_N8N_URL='https://flw.app.n8n.cloud/webhook/falaw-ia';
+window.SB_ADMIN_EMAIL='admin@falaw.com.br';
