943d949f236a6e8860bc50f0dad4b3f90e93147f
nevada-mirror <nevada@mirror.local>
2026-07-02 15:36:38 -0300

    faa

--- html/Falaw/falaw.com.br/sb-config.js
+++ html/Falaw/falaw.com.br/sb-config.js
@@ -3,7 +3,7 @@ window.SB_URL='https://yleofidqkimeanpuothv.supabase.co';
 window.SB_KEY='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlsZW9maWRxa2ltZWFucHVvdGh2Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3NDQ3Njc1MiwiZXhwIjoyMDkwMDUyNzUyfQ.j5ORA0kTEogVuzsDHh3YvneWrZMVKqyFRCkUjo7EF1k';
 window.BACKEND_API_URL='https://falaw-s1s1.onrender.com';
 window.SB_N8N_URL='https://flw.app.n8n.cloud/webhook/falaw-ia';
-window.SB_GAS_URL='https://script.google.com/macros/s/AKfycbyued20NtyyPSQy3UZm3eezyTAeZhbeG_hKsDEl9rDJhUc4r_mpM5gOICYFFt8RZTOF/exec';
+window.SB_GAS_URL='https://script.google.com/macros/s/AKfycbxv0kV35jnf7EGOhOYy7XFKmp8_g5Zif4XdzwgbZJrLjZCYUj0FicR-9lF67UnczfUi/exec';
 window.SB_ADMIN_EMAIL='admin@falaw.com.br';
 /* Sempre sincroniza localStorage com a URL canônica */
 (function(){try{localStorage.setItem('fa_backend_api_url','https://falaw-s1s1.onrender.com');}catch(e){}})();
